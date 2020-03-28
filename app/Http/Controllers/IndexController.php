94d0750492507fe60416f71e492384d51147b69d
Frank <mablat@ebay.com>
2020-03-28 08:15:09 +0800

    check datastructure

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Skill;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -9,8 +11,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $user = User::first();
-        dd($user->skills);
         return view('index');
     }
 }
